b34454b0831606929285d68f1c9b6e65a49fb6d0
hizkielT <hezekiel.feyissa@gmail.com>
2022-07-02 13:20:10 +0300

    created a local storage hook for our task tracker

--- src/components/task_header.js
+++ src/components/task_header.js
@@ -23,8 +23,71 @@ function TaskHeader(props) {
               overflow: 'auto'
           }}
       >
+        {/* <Paper
+          
+          sx={{
+            maxWidth: 'md',
+            m: 8,
+          }}
+        > */}
+        <Grid
+          container
+          sx={{
+            backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+            ? theme.palette.grey[500]
+            : theme.palette.grey[100],
+            flexGrow: 1,
+            overflow: 'auto',
+            display: 'flex',
+            flexDirection: 'row',
+            my: 2
+          }}
+        >
+          <Grid item
+            md={9} lg={9} xl={9} xs={8}
+          >
+            <Typography 
+              component="h1" 
+              variant="h4" 
+              sx={{
+                // flexGrow: 1, 
+                // m: 3, 
+                // textAlign: 'center'
+                px: '55%',
+                py: 2
+              }}
+              >
+              Tasks
+            </Typography>
+          </Grid>
+            <Grid item
+              md={3} lg={3} xl={3} xs={3}
+            >
+
+            <Button 
+              varinat="contained" 
+               sx={{ 
+                display: 'inline-block',
+                background: '#000',
+                color: '#fff',
+                border: 'none',
+                padding: '10px 20px',
+                margin: '10px',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                textDecoration: 'none',
+                fontSize: '15px',
+                fontFamily: 'inherit',
+               }}
+              >
+              Add
+            </Button>
+              </Grid>
+        </Grid>
+                {/* </Paper> */}
           {/* <Container maxWidth="lg" sx={{ mt: 4, mb: 4,}}> */}
-          <Paper
+          {/* <Paper
             md={12} lg={12} xl={12} xs={12}
               sx={{
                   display: 'flex',
@@ -54,7 +117,7 @@ function TaskHeader(props) {
               Add New Task
             </Button>
               </Grid>
-          </Paper>
+          </Paper> */}
           {/* </Container> */}
         </Box>
         </React.Fragment>
